54bcd5e33a207dfbad444ea2d48c50ab77091cf2
SmilerRyan <ryanthornton03@gmail.com>
2024-04-13 21:21:14 +0100

    move more into a single file

--- Native/StandardRights.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PortProxyGUI.Native
-{
-    internal enum StandardRights : uint
-    {
-        STANDARD_RIGHTS_REQUIRED = 0x000F0000,
-    }
-}
--- Utils/PortPorxyUtil.cs
+++ Utils/PortPorxyUtil.cs
@@ -10,6 +10,26 @@ namespace PortProxyGUI.Utils
 {
     public static class PortPorxyUtil
     {
+
+        internal enum ScmRights : uint
+        {
+        SC_MANAGER_CONNECT = 0x0001,
+        SC_MANAGER_CREATE_SERVICE = 0x0002,
+        SC_MANAGER_ENUMERATE_SERVICE = 0x0004,
+        SC_MANAGER_LOCK = 0x0008,
+        SC_MANAGER_QUERY_LOCK_STATUS = 0x0010,
+        SC_MANAGER_MODIFY_BOOT_CONFIG = 0x0020,
+
+        SC_MANAGER_ALL_ACCESS =
+            0x000F0000
+            | SC_MANAGER_CONNECT
+            | SC_MANAGER_CREATE_SERVICE
+            | SC_MANAGER_ENUMERATE_SERVICE
+            | SC_MANAGER_LOCK
+            | SC_MANAGER_QUERY_LOCK_STATUS
+            | SC_MANAGER_MODIFY_BOOT_CONFIG
+        }
+
         internal static readonly string ServiceName = "iphlpsvc";
         internal static readonly string ServiceFriendlyName = "IP Helper";
 
